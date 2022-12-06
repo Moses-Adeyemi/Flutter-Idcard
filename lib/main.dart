c3071e6d05d57e271926051c54d7051f504a688e
Moses_Adeyemi <oludareadeyemi52@gmail.com>
2022-12-06 09:06:33 +0000

    gjh

--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(color: Colors.white),
       ),
     );
   }
